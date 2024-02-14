ff2bb78497fe4ee7cee72d8904b78a9395c11820
agnesl10 <158211772+agnesl10@users.noreply.github.com>
2024-02-14 13:58:54 -0500

    sdfsf

--- factors.py
+++ factors.py
@@ -1,9 +1,9 @@
 def all_factors(n):
     factors = []
-    for i in range(2,n):
+    for i in range(2,n): #starts at 2 then continues. thought i was 0 so you have to start at 2 b/c 45%0 breaks down the code.
         if n%i == 0:
             factors.append(i)
-    return factors
+    return factors #return ends the code so you have to put it in the function w/o
         
 number = int(input("enter a number: "))
 list_of_factors = all_factors(number)
--- gcf.py
+++ gcf.py
@@ -1,11 +1,11 @@
 x = int(input("number 1: "))
 y = int(input("number 2: "))
-factor = []
+factors = []
 def gcf(x,y):
-  global gcf
   for i in range(2,y):
       if x%i == 0 and y%i == 0:
-            factor.append
-      return factor
+            factors.append(i)
+gcf(x,y)#REMEMBER TO RUN FUNCTION
+print("gcf is: ", factors[-1])
 
-print gcf()
+#SAVE BEFORE YOU RUN
