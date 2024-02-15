cf907becc845337fd73a589e0210c9381c57d5b9
agnesl10 <158211772+agnesl10@users.noreply.github.com>
2024-02-15 14:09:55 -0500

    dfsdf

--- numgame.py
+++ numgame.py
@@ -1,4 +1,13 @@
-import random
-number = random ()
-
- 
+import random 
+number = random.choice([1,2,3,4,5,6,7,8,9,10])
+(x) = int(input("Guess: "))
+while (x) != number:
+    if x < number:
+        print("try again; it's greater")
+     
+    elif x > number:
+        print("try again; its smaller")
+    
+    int(input("guess again: "))  
+else:
+    print("correct")
